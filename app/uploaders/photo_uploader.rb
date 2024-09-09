0341bb5b9cf0b24fd733c70c1dda2ddf50d11dfb
Diether <therdbear8@gmail.com>
2024-09-09 20:18:47 +0800

    Fixed bugs and Updated Styling for Posts

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -33,6 +33,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
   #   process resize_to_fit: [50, 50]
   # end
 
+  # version :square do
+  #   process resize_to_fill: [300, 300] # You can adjust the size
+  # end
+
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_allowlist
